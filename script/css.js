1804d45c8be6ad27f59dba02aafad36f0de90bd0
BBS007 <bbs07@laposte.net>
2014-03-28 17:59:08 +0100

    Style changing to night mode working

--- script/css.js
+++ script/css.js
@@ -1,17 +1,11 @@
-var button1 = window.document.getElementById("styleNormal");
-button1.addEventListener(onclick, styleDefault());
-
-var button2 = window.document.getElementById("styleNuit");
-button2.addEventListener(onclick, styleNuit());
-
 var stylesheet = window.document.getElementById("style");
 
 function styleDefault(){
 	stylesheet.href = "./stylesheet/stylesheet.css";
 	//document.styleSheets = "./stylesheet/stylesheet.css";
-};
+}
 
 function styleNuit() {
 	stylesheet.href = "./stylesheet/stylesheet_nuit.css";
 	//document.styleSheets = "./stylesheet/stylesheet_nuit.css";
-};
+}
