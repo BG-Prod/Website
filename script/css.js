478559ed7a3bae25cccb70787a294255bd360795
Benoît Garçon <benoit.garconbesson@gmail.com>
2014-04-15 13:56:06 +0200

    new theme

--- script/css.js
+++ script/css.js
@@ -10,11 +10,27 @@ button1.onclick = styleDefault;
 var button2 = window.document.getElementById("styleNuit");
 button2.onclick = styleNuit;
 
+//Bouton du style original
+var button3 = window.document.getElementById("styleOrigin");
+button3.onclick = styleOrigin;
+
+//Bouton reset
+var button4 = window.document.getElementById("styleReset");
+button4.onclick = styleReset;
+
 //Fonctions de changement de style
 function styleNuit() {
 	stylesheet.href = "./stylesheet/stylesheet_nuit.css";
-}
+};
 
 function styleDefault(){
 	stylesheet.href = "./stylesheet/stylesheet.css";
-}
+};
+
+function styleOrigin() {
+	stylesheet.href = "./stylesheet/stylesheet.css";
+};
+
+function styleReset() {
+	stylesheet.href = "./stylesheet/reset.css";
+};
