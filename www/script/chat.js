da5abee715ce0db3f69fbd2a597fcd0e57583b88
BBS007 <bbs07@laposte.net>
2014-05-24 16:11:19 +0200

    chat #2

--- www/script/chat.js
+++ www/script/chat.js
@@ -3,37 +3,37 @@
 	Envoi de message
 
 ***************************************/
-// Bouton d'envoi du message
-var bEnvoyer = document.getElementById("post");
+(function(){
 
-bEnvoyer.addEventListener("click", function() {
-	var xhr; 
-	try {  xhr = new ActiveXObject('Msxml2.XMLHTTP');   }
+// Bouton d'envoi du message
+var chat = document.getElementById("chatsend");
+
+chat.onsubmit = function() {
+	var req; 
+	try {  req = new ActiveXObject('Msxml2.XMLHTTP');   }
 	catch (e) 
 	{
-		try {   xhr = new ActiveXObject('Microsoft.XMLHTTP'); }
+		try {   req = new ActiveXObject('Microsoft.XMLHTTP'); }
 		catch (e2) 
 		{
-		   try {  xhr = new XMLHttpRequest();  }
-		   catch (e3) {  xhr = false;   }
+		   try {  req = new XMLHttpRequest();  }
+		   catch (e3) {  req = false;   }
 		}
 	}
 	
-	xhr.onreadystatechange  = function() { 
-		   if(xhr.readyState  == 4){
-			if(xhr.status  == 200) 
-				document.ajax.dyn="Received:"  + xhr.responseText; 
-			else
-				document.ajax.dyn="Error code " + xhr.status;
-			}
-		};
-		
-	xhr.open( "POST", "./htbin/chatsend.py",  true);
+	req.onreadystatechange  = function() { 
+	   if(req.readyState  == 4 && req.status  == 200)
+			document.ajax.dyn="Received:"  + req.responseText; 
+		else
+			document.ajax.dyn="Error code " + req.status;
+	};
+	
+	req.open( "POST", "./htbin/chatsend.py",  true);
+	var message = getElementById("msg");
 	   
-	   var message = getElementById("msg");
+	req.send("num=0" + "&" + "msg=" + msg.innerText);  
 	   
-	   xhr.send("num=0" + "&" + "msg=" + msg.innerText);
-	   
-	   
-	   return false;
-});
+	return false;
+};
+
+}());
