ef5287804560d811f34209967c9ae1401c3a87c8
BBS007 <benji@benji-virtual-machine.(none)>
2014-05-24 16:58:09 +0200

    message posting without error code

--- www/script/chat.js
+++ www/script/chat.js
@@ -5,43 +5,12 @@
 ***************************************/
 (function(){
 
-// Bouton d'envoi du message
-var chat = document.getElementById("chatsend");
+	$('#post').click(function(e){
+		e.preventDefault();
 
-chat.onsubmit = function() {
-	var req; 
-	try {  req = new ActiveXObject('Msxml2.XMLHTTP');   }
-	catch (e) 
-	{
-		try {   req = new ActiveXObject('Microsoft.XMLHTTP'); }
-		catch (e2) 
-		{
-		   try {  req = new XMLHttpRequest();  }
-		   catch (e3) {  req = false;   }
-		}
-	}
-	
-	var params = "num=0&msg=";
-	
-	var message = getElementById("msg");
-	var params = params + message.innerText;
-	
-	req.open("POST", "./htbin/chatsend.py", true);
-	
-	req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	req.setRequestHeader("Content-length", params.length);
-	req.setRequestHeader("Connection", "close");	
-	
-	req.onreadystatechange  = function() { 
-   if(req.readyState  == 4 && req.status  == 200)
-		document.ajax.dyn="Received:"  + req.responseText; 
-	else
-		document.ajax.dyn="Error code " + req.status;
-	};
+		var message = document.getElementById('msg').value;
 
-	req.send(params);  
-	   
-	return false;
-};
+		$.ajax({url : "./htbin/chatsend.py", type : "POST", data : "num="+"0"+"&msg="+message});
+	});
 
 }());
